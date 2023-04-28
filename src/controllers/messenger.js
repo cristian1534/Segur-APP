e6930d9ff5262d89c2bccbb1f7afb6c81d14fb69
Cristian Machuca <cmachuca32@gmail.com>
2023-04-28 17:41:31 +0000

    Uploading Messenger

--- src/controllers/messenger.js
+++ src/controllers/messenger.js
@@ -4,15 +4,12 @@ exports.send_message = (array, address, info) => {
   const accountSid = process.env.ACCOUNT_SID;
   const authToken = process.env.AUTH_TOKEN;
   const client = require("twilio")(accountSid, authToken);
-
-  array.map((number) => {
-    console.log(`${number} ${address} ${info}`)
-    client.messages
-      .create({
-        body: `Alerta: ${address} ${info}`,
-        from: "+16206340387",
-        to: number,
-      })
-      .then((message) => console.log(message.sid));
-  });
+ 
+  client.messages
+    .create({
+      body: `Alerta: ${address} ${info}`,
+      from: "+16206340387",
+      to: array,
+    })
+    .then((message) => console.log(message.sid));
 };
